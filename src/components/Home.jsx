39ee8fbf45645d10fd2ecb4a7af5687134e87d73
BennyZHill <ben.h@builtbypixel.com>
2021-08-17 10:00:07 +0100

    refactor

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const Home = () => {
+  return (
+    <div>
+      <h1 className="title">Travel Japan</h1>
+      <div className="home-image" />
+    </div>
+  );
+};
+
+export default Home;
