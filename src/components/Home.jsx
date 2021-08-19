27e3e84998f50b49d6fd017a2056b8b2058dbefc
BennyZHill <ben.h@builtbypixel.com>
2021-08-19 12:34:31 +0100

    rearange folders

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,9 @@ const Home = () => {
   return (
     <div>
       <h1 className="title">Travel Japan</h1>
+      <h2 className="subtitle">Welcome to your adventure</h2>
       <div className="home-image" />
+      <p className="text"></p>
     </div>
   );
 };
